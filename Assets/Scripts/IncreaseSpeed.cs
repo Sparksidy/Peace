4154f0626681fe138263da46276570f2d432aa67
Siddharth Gupta <sidystan@gmail.com>
2015-10-08 00:28:50 +0530

    Level 1 completed

---
+++ Assets/Scripts/IncreaseSpeed.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class IncreaseSpeed : MonoBehaviour {
+	
+
+	public void OnTriggerEnter2D(Collider2D  other){
+		var player = other.GetComponent<Player>();
+		player.MaxSpeed = 8;
+	}
+}
